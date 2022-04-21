7d041942683f03dda25ab6f8d7e040ded79095e1
Kerriea-star <ojared511@gmail.com>
2022-04-21 11:17:16 +0300

    Finalized on serializers

--- api/serializers.py
+++ api/serializers.py
@@ -14,3 +14,21 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('username', 'first_name', 'last_name', 'email', 'age', 'bio'
         'birth_place', 'school', 'occupation', 'what_are_you_seeking_on_site',
         'is_active')
+
+class CustomRegisterSerializer(RegisterSerializer):
+    email = serializers.EmailFiels(required=True)
+    password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    age = serializers.DateField(required=True)
+
+    def get_cleaned_data(self):
+        super(CustomRegisterSerializer, self).get_cleaned_data()
+
+        return {
+            'password': self.validated_data.get('password', ''),
+            'email': self.validated_data.get('email', ''),
+            'first_name': self.validated_data.get('first_name', ''),
+            'last_name': self.validated_data.get('last_name', ''),
+            'age': self.validated_data.get('age', ''),
+        }
